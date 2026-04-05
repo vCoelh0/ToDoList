20dcc6505e3b7b58e07253de5618792d7d1dca61
vCoelh0 <coelhocomercial2k@gmail.com>
2026-04-05 18:20:52 -0300

    Metodo Deletar Task

--- backend-java/src/main/java/com/bgdev/todolist/controllers/TasksController.java
+++ backend-java/src/main/java/com/bgdev/todolist/controllers/TasksController.java
@@ -3,6 +3,8 @@ package com.bgdev.todolist.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,11 @@ public class TasksController {
 		TasksDTO newTask = service.create(dto, userId);
 		return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable Long id) {
+		service.delete(id);
 	}
 	
 }
--- backend-java/src/main/java/com/bgdev/todolist/entities/services/TasksService.java
+++ backend-java/src/main/java/com/bgdev/todolist/entities/services/TasksService.java
@@ -35,4 +35,8 @@ public class TasksService {
 			
 	}
 	
+	public void delete(Long id) {
+		taskRepository.deleteById(id);
+	}
+	
 }
